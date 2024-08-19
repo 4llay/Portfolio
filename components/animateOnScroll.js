94ac248dcdf08a0ba3c9609a237b78988dedd03b
unknown <4llayjpcg@gmail.com>
2024-08-19 10:54:14 -0300

    still merging...

--- components/animateOnScroll.js
+++ components/animateOnScroll.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 
 const observer = new IntersectionObserver((entries) => {
@@ -15,21 +14,3 @@ const hiddenElements = document.querySelectorAll('.animateOnScroll');
 hiddenElements.forEach((el) => observer.observe(el))
 
 
-=======
-'use client'
-
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add(`mostrar`)
-        } else {
-            entry.target.classList.remove('mostrar')
-        }
-    })
-});
-
-const hiddenElements = document.querySelectorAll('.animateOnScroll');
-hiddenElements.forEach((el) => observer.observe(el))
-
-
->>>>>>> b7e050231fd2f52901f2694ec368c349b8f0fd42
